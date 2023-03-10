1a2ec9c54c35722aee1f8df4c170618bb242e738
mlietti1 <mcf.lietti@outlook.com>
2023-03-10 15:52:18 +0100

    product

---
+++ src/org/lessons/java/shop/Main.java
@@ -0,0 +1,21 @@
+package org.lessons.java.shop;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Product.getIva());
+        Product spaghetti = new Product("Spaghetti");
+        spaghetti.setPrice(1.20);
+        spaghetti.setDescription("tempo di cottura 10min");
+
+        System.out.println(spaghetti);
+        System.out.println("Product name: " + spaghetti.getName() + ", price: " + spaghetti.getPrice() + ", final price: " + spaghetti.getFinalPrice() + ", code: " + spaghetti.getCode() + ", description: " + spaghetti.getDescription());
+        System.out.println(spaghetti.getFullName());
+
+        Product pomodori = new Product();
+        pomodori.setName("Pomodori pachino");
+        pomodori.setPrice(3.45);
+
+
+        System.out.println("Product name: " + pomodori.getName() + ", price: " + pomodori.getFinalPrice() + ", code: " + spaghetti.getCode());
+    }
+}
---
+++ src/org/lessons/java/shop/Product.java
@@ -0,0 +1,81 @@
+package org.lessons.java.shop;
+
+import java.text.DecimalFormat;
+import java.util.Random;
+
+public class Product {
+
+    //Campi
+    private final int code;
+    private String name;
+    private String description;
+    private double price;
+    private static final double IVA = 0.21;
+
+    // Costruttori
+    public Product(){
+        Random rand = new Random();
+        code = rand.nextInt(1, 10000000);
+        name = "New product";
+    }
+
+    public Product(String name){
+        Random rand = new Random();
+        code = rand.nextInt(1, 10000000);
+        this.name = name;
+    }
+
+    // Getter e setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static double getIva() {
+        return IVA;
+    }
+
+    // Metodi
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "code=" + code +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    public String getFinalPrice(){
+        DecimalFormat decimalFormat = new DecimalFormat("##.00");
+        return decimalFormat.format(price * (1 + IVA));
+    }
+
+    public String getFullName(){
+        return name + "-" + code;
+    }
+}
